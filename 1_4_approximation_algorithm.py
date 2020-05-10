1953fca798fd161da3cdd749bbc43c4405ddf026
Jana Puschmann <jana.puschmann@post.au.dk>
2020-05-10 13:22:02 +0200

    Fix matching after misunderstanding.

--- 1_4_approximation_algorithm.py
+++ 1_4_approximation_algorithm.py
@@ -31,39 +31,52 @@ def odd(S):
 
 def match(S):
     """ Match even's from the left with odd's from the right and vice versa """
-    n = len(S)
-    matches = np.zeros((n, n), dtype=int)
+    matches_left = np.zeros((len(S), len(S)), dtype=int)
+    matches_right = np.zeros((len(S), len(S)), dtype=int)
 
-    front = 0
-    back  = len(S) - 1 # counters for traversing the string
-
+    # counters for traversing the string
+    front = 0 # even
+    back = len(S) - 1 if len(S) % 2 == 0 else len(S) - 2 # odd
+    count_left = 0
     while front <= 1/2 * len(S) or back >= 1/2 * len(S):
         if S[front] == "h" and S[back] == "h":
-            if front % 2 == 1 and back % 2 == 0:
-                # print(front, back)
-                matches[front, back] = 1
-                matches[back, front] = 1
-                front += 1
-                back  -= 1
-            elif front % 2 == 0 and back % 2 == 1:
-                # print(front, back)
-                matches[front, back] = 1
-                matches[back, front] = 1
-                front += 1
-                back  -= 1
-            else:
-                front += 1
+            # evens from the left, with odds from the right
+            matches_left[front, back] = 1
+            matches_left[back, front] = 1
+            front += 2
+            back  -= 2
+            count_left += 1
         elif S[front] == "h":
-            back -= 1
+            back -= 2
         else:
-            front += 1
+            front += 2
 
-    return matches
+    front = 1 # odd
+    back = len(S) - 2 if len(S) % 2 == 0 else len(S) - 1 # even
+    count_right = 0
+    while front <= 1/2 * len(S) or back >= 1/2 * len(S):
+        if S[front] == "h" and S[back] == "h":
+            # odds from the left, with evens from the right
+            matches_right[front, back] = 1
+            matches_right[back, front] = 1
+            front += 2
+            back  -= 2
+            count_right += 1
+        elif S[front] == "h":
+            back -= 2
+        else:
+            front += 2
+
+    if count_left >= count_right:
+        return matches_left
+    else:
+        return matches_right
 
 
 def main():
     # match odd's and even's
-    matches = match(S)
+    matching = match(S)
+    print(matching)
 
     pass
 
